54e791d342a864b6fa5e4ef90d148d55e70b01f6
Mtduffey36 <michael.t.duffey@gmail.com>
2024-05-27 19:44:15 -0500

    feat: dnd API
    added api called for human,dward,elf, and halfling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,6 @@
 const buttonEL = document.getElementById('clickMe');
 const information = document.getElementById('information');
 
-// const wizardLevels = function() {
-//     fetch('https://www.dnd5eapi.co/api/classes/wizard/levels')
-//     .then(function(response) {
-//         console.log(response);
-//         return response.json();
-//     })
-//     .then(function(wizard) {
-//         console.log("after json()", wizard);
-//         console.log('cantrips?', wizard[0].spellcasting.cantrips_known);
-//         //the for loop below loops through spell slots and cantrips for wizard levels 1-20
-//         for(let i = 0; i < 19; i++) {
-//             console.log(wizard[i].cantrips_known);
-//             const h3Tags = document.createElement('h3');
-//             h3Tags.textContent = wizard[0].spellcasting.cantrips_known;
-//             information.append(h3Tags);
-//         }
-//     })
-// }
-
-
-// buttonEL.addEventListener('click', wizardLevels);
-
 const humanStats = function() {
     fetch('https://www.dnd5eapi.co/api/races/human')
     .then(function(response) {
@@ -31,17 +9,100 @@ const humanStats = function() {
     .then(function(human) {
         console.log("After json", human);
 
-        const h3Tag = document.createElement('h3');
-        const pTag = document.createElement('p');
-
-        h3Tag.textContent = human.index;
-        pTag.textContent = human.starting_proficiencies.length
-
-        information.append(h3Tag);
-        h3Tag.append(pTag);
+        //change variable def based on layout of future HTML
+        const informationArea = document.getElementById('information');
+        const name = document.createElement('h3');
+        const prof = document.createElement('li');
+        const lang = document.createElement('li');
 
 
+        name.textContent = 'Race: ' + human.index;
+        prof.textContent = 'Proficiencies: ' + human.starting_proficiencies.length;
+        lang.textContent = 'Languages: ' + human.languages[0].index;
+
+        //rearrange append information based on changed variables above
+        informationArea.append(name);
+        name.append(prof);
+        name.append(lang);
+    })
+}
+
+const dwarfStats = function() {
+    fetch('https://www.dnd5eapi.co/api/races/dwarf')
+    .then(function(response) {
+        return response.json()
+    })
+    .then(function(dwarf) {
+        console.log("After json", dwarf);
+
+        //change variable def based on layout of future HTML
+        const informationArea2 = document.getElementById('information2');
+        const name = document.createElement('h3');
+        const prof = document.createElement('li');
+        const lang = document.createElement('li');
+
+        name.textContent = 'Race: ' + dwarf.index;
+        prof.textContent = 'Proficiencies: ' + dwarf.starting_proficiencies.length;
+        lang.textContent = 'Languages: ' + dwarf.languages[0].index + ', ' + dwarf.languages[1].index
+
+        //rearrange append information based on changed variables above
+        informationArea2.append(name);
+        name.append(prof);
+        name.append(lang);
+    })
+}
+
+const elfStats = function() {
+    fetch('https://www.dnd5eapi.co/api/races/elf')
+    .then(function(response) {
+        return response.json()
+    })
+    .then(function(elf) {
+        console.log("After json", elf);
+
+        //change variable def based on layout of future HTML
+        const informationArea3 = document.getElementById('information2');
+        const name = document.createElement('h3');
+        const prof = document.createElement('li');
+        const lang = document.createElement('li');
+
+        name.textContent = 'Race: ' + elf.index;
+        prof.textContent = 'Proficiencies: ' + elf.starting_proficiencies.length;
+        lang.textContent = 'Languages: ' + elf.languages[0].index + ', ' + elf.languages[1].index
+
+        //rearrange append information based on changed variables above
+        informationArea3.append(name);
+        name.append(prof);
+        name.append(lang);
+    })
+}
+
+const halflingStats = function() {
+    fetch('https://www.dnd5eapi.co/api/races/halfling')
+    .then(function(response) {
+        return response.json()
+    })
+    .then(function(halfling) {
+        console.log("After json", halfling);
+
+        //change variable def based on layout of future HTML
+        const informationArea4 = document.getElementById('information2');
+        const name = document.createElement('h3');
+        const prof = document.createElement('li');
+        const lang = document.createElement('li');
+
+        name.textContent = 'Race: ' + halfling.index;
+        prof.textContent = 'Proficiencies: ' + halfling.starting_proficiencies.length;
+        lang.textContent = 'Languages: ' + halfling.languages[0].index + ', ' + halfling.languages[1].index
+
+        //rearrange append information based on changed variables above
+        informationArea4.append(name);
+        name.append(prof);
+        name.append(lang);
     })
 }
 
 buttonEL.addEventListener('click', humanStats);
+buttonEL.addEventListener('click', dwarfStats);
+buttonEL.addEventListener('click', elfStats);
+buttonEL.addEventListener('click', halflingStats);
